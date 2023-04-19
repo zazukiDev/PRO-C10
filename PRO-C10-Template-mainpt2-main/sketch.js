1d34390e3f7a3e03256aa955850e365b73d74b33
zazukiDev <130075009+zazukiDev@users.noreply.github.com>
2023-04-19 12:47:09 -0300

    Add files via upload

---
+++ PRO-C10-Template-mainpt2-main/sketch.js
@@ -0,0 +1,36 @@
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  seaImg = loadImage("sea.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
+}
+
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+
+  if(sea.x < 0){
+    sea.x = sea.width/8;
+    
+  }
+
+ 
+  drawSprites();
+}
